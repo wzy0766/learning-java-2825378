e694c9ec6e93cc7c53c5e1c4b1de5d21ebe2ea1c
Zeyu Wang <zwang716@usc.edu>
2021-01-07 20:08:38 -0800

    Edited by Robert

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        int StudentAge = 15;
+        double StudentGPA = 3.45;
+        char StudentFirstInitial = 'Robert';
+        char StudentLastInitial = 'Wang';
+        boolean hasPerfectAttendatence = true;
+
+        System.out.println(StudentAge);
+        System.out.println(StudentGPA);
+        System.out.println(StudentFirstInitial);
+        System.out.println(StudentLastInitial);
+        System.out.println(hasPerfectAttendatence);
     }
 }
