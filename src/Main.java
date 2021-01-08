56ebd062c21caac851bf82992c4b78ca6caaf5d3
Zeyu Wang <zwang716@usc.edu>
2021-01-07 20:15:03 -0800

    R

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,17 @@ public class Main {
         char studentFirstInitial = 'K';
         char studentLastInitial = 'H';
         boolean hasPerfectAttendance = true;
+        String StudentName = "Robert Wang";
+        String FirstName = "Robert";
+        String LastName = "Wang";
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
+        System.out.println(StudentName);
+        System.out.println(FirstName);
+        System.out.println(LastName);
     }
 }
