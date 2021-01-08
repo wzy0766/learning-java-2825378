306fc1a3db44260c92196d139c82190f6a2b9db6
Zeyu Wang <zwang716@usc.edu>
2021-01-07 23:52:30 -0800

    m

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
+        Triangle triangleB = new Triangle(3, 2.5, 9, 3, 3);
     }
 
 }
