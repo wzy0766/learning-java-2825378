a95c499ac18e38b9fcf1c4555063ec149749c962
Zeyu Wang <zwang716@usc.edu>
2021-01-08 00:11:55 -0800

    m

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
-        Triangle triangleB = new Triangle(3, 2.598, 3, 3,3);
+        Triangle triangleB = new Triangle(3, 2.598, 3, 3, 3);
 
         double triangleAArea = triangleA.findArea();
         System.out.println(triangleAArea);
@@ -11,7 +11,10 @@ public class Main {
         System.out.println(triangleBArea);
 
         // Triangle.findArea()? --> Math.pow(2,3)?
+        System.out.println(triangleA.sideLenThree);
+        System.out.println(triangleB.base);
 
+        System.out.println(Triangle.numOfSides);
 
     }
 
