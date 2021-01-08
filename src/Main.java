c800ff35930b01a2e91cbe0f2c57fdc33d2d721e
Zeyu Wang <zwang716@usc.edu>
2021-01-07 21:12:24 -0800

    Control Flow

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,38 @@
+import java.util.Scanner;
+
+import org.graalvm.compiler.asm.aarch64.AArch64Assembler.SystemRegister;
 
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
+        String question = "What is the result of one plus one?";
+        String choiceOne = "Zero";
+        String choiceTwo = "One";
+        String choiceThree = "Two";
 
-        String correctAnswer = choiceTwo;
+        String correctAnswer = choiceThree;
 
         // Write a print statement asking the question
+        System.out.println(question);
         // Write a print statement giving the answer choices
-
+        System.out.println("Choose one of the following: " + choiceOne + ", " + choiceTwo + ", or " + choiceThree + ".");
         // Have the user input an answer
+        Scanner input = new Scanner(System.in);
+        String userinput = input.next();
         // Retrieve the user's input
+        System.out.println("Your choice is: " + userinput);
 
         // If the user's input matches the correctAnswer...
         // then the user is correct and we want to print out a congrats message to the user.
+        if (userinput.equals(correctAnswer)) {
+            System.out.println("You are correct! ");
+        }
 
         // If the user's input does not match the correctAnswer...
         // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
-
+        else {
+            System.out.println("You are incorrect! The correct answer is " + choiceThree + ".");
+        }
     }
 
 }
