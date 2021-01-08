d12cc2daf5e32b159cc8214c5185aa93db3af57e
Zeyu Wang <zwang716@usc.edu>
2021-01-07 23:55:59 -0800

    v

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ public class Main {
         Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
         Triangle triangleB = new Triangle(3, 2.598, 3, 3,3);
 
+        double triangleA_Area = triangleA.findArea();
+        System.out.println(triangleA_Area);
+
+        double triangleB_Area = triangleB.findArea();
+        System.out.println(triangleB_Area);
     }
 
 }
