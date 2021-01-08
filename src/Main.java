4d1051bb86fa99fd16b952c809a0a996c636b5e6
Zeyu Wang <zwang716@usc.edu>
2021-01-07 20:47:26 -0800

    C

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean isOnRepeat = true;
+        while (isOnRepeat) {
+            System.out.println("The current song is repeating!");
+            System.out.println("Would you like to stop repeating thie song? If so, answer yes");
+            
+            String userInput = input.next();
+
+            if (userInput.equals("yes")) {
+                isOnRepeat = false;
+            }
+        }
+        System.out.println("Playing next song！");
+
     }
 }
